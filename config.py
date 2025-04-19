c13873b6608ead792580c08b028efbc5e8f9aae4
Maximun <132590174+max-4-3@users.noreply.github.com>
2025-04-19 12:13:25 +0530

    Stop Tracking config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import asyncio, os
-
-DOWNLOAD_PATH = os.path.expanduser('~/Downloads/Videos')
-DOWNLOAD_SEM = asyncio.Semaphore(2)
-QUERY_SEM = asyncio.Semaphore(3)
-INITIAL_PATH = os.path.join(DOWNLOAD_PATH, 'Initials')
-os.makedirs(DOWNLOAD_PATH, exist_ok=True)
-os.makedirs(INITIAL_PATH, exist_ok=True)
