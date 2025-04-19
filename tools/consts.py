9e058275b0c78d28dafe364e983bd81d79129903
Maximun <132590174+max-4-3@users.noreply.github.com>
2025-04-19 12:16:58 +0530

    Added a Reasonable Log Path

--- tools/consts.py
+++ tools/consts.py
@@ -1,6 +1,6 @@
 import os
 
-LOG_PATH = os.path.expanduser("~/Downloads/Videos/Logs")
+LOG_PATH = os.path.join(os.path.dirname((os.path.dirname(__file__))), "Logs")
 os.makedirs(LOG_PATH, exist_ok=True)
 LOG_FORMAT = "[%(asctime)s] [%(levelname)s] [PID:%(process)d] [%(threadName)s] [%(funcName)s@%(filename)s:%(lineno)d] - %(message)s"
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
