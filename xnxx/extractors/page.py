47824b5fdb56298041d9016e26c5d0a944e89321
Maximun <132590174+max-4-3@users.noreply.github.com>
2025-04-25 21:15:34 +0530

    Changed page.py

--- xnxx/extractors/page.py
+++ xnxx/extractors/page.py
@@ -2,24 +2,31 @@ from bs4 import BeautifulSoup, Tag
 from rich import print
 import asyncio, aiohttp
 
+def get_text(elem):
+    try:
+        return elem.text
+    except:
+        return "FiledNotFound!"
+
 async def parse_video_element(video_element: Tag) -> dict | None:
     try:
         id = int(video_element.attrs.get('data-id') or 0)
-        title = video_element.select_one('div.thumb-under p a').text
+        title = get_text(video_element.select_one('div.thumb-under p a'))
         url = "https://xnxx.health" + video_element.select_one('div.thumb-under p a').attrs.get('href', '')
         thumbnail = video_element.select_one('div.thumb img').attrs.get('data-src')
 
         right = video_element.select_one('span.right')
-        views = right.contents[0].text.strip()
-        superfluous = right.select_one('span.superfluous').text.strip()
-        duration = video_element.select_one('p.metadata').contents[1].text.strip()
+        views = get_text(right.contents[0]).strip()
+        superfluous = get_text(right.select_one('span.superfluous')).strip()
+        duration = get_text(video_element.select_one('p.metadata').contents[1]).strip()
 
         try:
-            is_hd = int(video_element.select_one('span.video-hd').contents[1].strip().replace("p", "")) >= 720
+            is_hd = int(get_text(video_element.select_one('span.video-hd').contents[1]).strip().replace("p", "")) >= 720
         except:
             is_hd = False
         
         return {
+            'id': id,
             "title": title,
             "thumbnail": thumbnail,
             "duration": duration,
